b2aca5fb70aa5413fdc68bf511979d5f502a6fd6
iterable-marketing <gabriel.ortiz@iterable.com>
2020-06-09 16:17:25 -0700

    minor style and feature requests to footer.php and image blocks

--- footer.php
+++ footer.php
@@ -5,10 +5,20 @@
  * Comtains closing divs for header.php.
  *
  * For more info: https://developer.wordpress.org/themes/basics/template-files/#template-partials
- */			
- ?>
+ */
+	
+	$options = get_fields('options');
+	
+	$menu_img_id    = array_search_key( 'kw_settings__menu_featured_image', $options );
+	?>
 					
 				<footer id="kw-c-footer" class="footer" role="contentinfo">
+					
+					<?php if( $menu_img_id ): ?>
+
+							<?php echo wp_get_attachment_image( $menu_img_id, 'full', true , array( 'class' => 'kw-c-footer__img' ) ); ?>
+
+					<?php endif; ?>
 					
 					<div class="grid-container">
 						<div class="inner-footer grid-x grid-margin-x grid-padding-x">
@@ -21,6 +31,7 @@
 
 							<div class="small-12 medium-12 large-12 cell">
 								<p class="source-org copyright kw-u-mt-2 kw-u-weight-bold">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+								<p class="kw-u-font-size-sm"><?php echo get_bloginfo('description'); ?></p>
 							</div>
 
 						</div> <!-- end #inner-footer -->
